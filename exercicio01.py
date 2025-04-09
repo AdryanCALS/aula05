9a53ea0b066b61c9e3452c7889d3f8344c8b25b8
Adryan Lima <acals@poli.br>
2025-04-09 14:15:05 -0300

    primeiros Exercicios

---
+++ exercicio01.py
@@ -0,0 +1,22 @@
+# for i in range(1,11):
+#     print(i)
+# i = 1
+# while i <= 10:
+#     print(i, end = " ")
+#     i += 1
+# for i in range(10,0,-1):
+#     print(i, end=" ")
+#Triangulo retangulo:
+# for i in range(1,10):
+#     for j in range(1,i):
+#         print("*", end=" ")
+#     print()
+#triangulo retangulo
+# for i in range(1,11):
+#     for k in range(1,i):
+#         print("*", end=" ")
+#     print()
+#triangulo numeros
+for i in range(1,5):
+    for j in range(1,i):
+        print
---
+++ exercicio04.py
@@ -0,0 +1,6 @@
+i = int(input("Digite o numero: "))
+if i == 0:
+    print("Favor insira um numero maior que zero!")
+else:
+    for x in range(1,i+1):
+        print(x)
